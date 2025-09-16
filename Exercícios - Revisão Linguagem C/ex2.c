a059e461fc73cbf229334d9abc38f57222c8f220
Laiza Milena da Silva <laiza.milena.silva@gmail.com>
2025-09-16 17:01:14 -0300

    ex2.c

--- Exercícios - Revisão Linguagem C/ex2.c
+++ Exercícios - Revisão Linguagem C/ex2.c
@@ -4,17 +4,22 @@
 int main()
 {
     int n;
+    
+    do
+    {
+        printf("Insira o tamanho do vetor (tamanho mínimo 2): \n");
+        scanf("%d", &n);
 
-    printf("Insira o tamanho do vetor: \n");
-    scanf("%d", &n);
+    } while (n < 2);
+    
     int *v = (int *)malloc(n * sizeof(int));
-
+    
     if (v == NULL)
     {
         printf("Erro ao alocar na memória\n");
         return 1;
     }
-
+    
     for (int i = 0; i < n; i++)
     {
         printf("insira o %d° valor: ", i + 1);
@@ -24,15 +29,16 @@ int main()
     for (int i = 0; i < n; i++)
     {
         int produto = 1;
-    
+
         for (int j = 0; j < n; j++)
         {
-            if (i != j){
+            if (i != j)
+            {
                 produto *= v[j];
             }
         }
 
-        printf ("%d ", produto);
+        printf("%d ", produto);
     }
 
     free(v);
