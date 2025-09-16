e33568bff772f5dce7167b9a0e7b676b108ff1fa
Laiza Milena da Silva <laiza.milena.silva@gmail.com>
2025-09-16 15:49:09 -0300

    todos.c

---
+++ Exercícios - Revisão Linguagem C/todos.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void ex1(int n, int k)
+{
+    int v[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("insira o %d° valor: ", i + 1);
+        scanf("%d", &v[i]);
+    }
+
+    int soma = 0, verifica = 0;
+
+    for (int i = 0; i < n && verifica != 1; i++)
+    {
+        for (int j = 0; j < n && verifica != 1; j++)
+        {
+            if (i != j)
+            {
+                soma = v[i];
+                soma += v[j];
+            }
+            
+            printf("%d\n\n", soma);
+
+            if (soma == k)
+            {
+                printf("True\n");
+                verifica = 1;
+            }
+        }
+    }
+
+    if (verifica == 0){
+        printf ("False\n");
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", v[i]);
+    }
+}
+
+int main()
+{
+    int op;
+
+    printf("Escolha uma das opções abaixo utilizando somente o número de identificação: \n");
+    printf("1  - ex1 \n2  - ex2 \n3  - ex3 \n4  - ex4 \n5  - ex5 \n6  - ex6 \n7  - ex7 \n8  - ex8 \n9  - ex9 \n10 - ex10 \n");
+    scanf("%d", &op);
+
+    switch (op)
+    {
+    case 1:
+        int n, k;
+
+        printf("Insira o tamanho do vetor: \n");
+        scanf("%d", &n);
+        printf("Insira o valor a verificar se existe no vetor: \n");
+        scanf("%d", &k);
+
+        ex1(n, k);
+
+        break;
+
+    default:
+        break;
+    }
+}
