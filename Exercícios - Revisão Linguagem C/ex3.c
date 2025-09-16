5d7b8bcedc38477f9499f22863ad1488948afa1e
Laiza Milena da Silva <laiza.milena.silva@gmail.com>
2025-09-16 17:25:11 -0300

    ex3.c

--- Exercícios - Revisão Linguagem C/ex3.c
+++ Exercícios - Revisão Linguagem C/ex3.c
@@ -5,13 +5,20 @@ int main()
 {
     int n;
 
-        printf("Insira um número para verificação: \n");
-        scanf("%d", &n);
+    printf("Insira um número para verificação: \n");
+    scanf("%d", &n);
 
-    if (((n % 2) != 0 && (n % 3) != 0 && (n % 5) != 0 && (n % 7) != 0) || n == 2 || n == 3 || n == 5 || n == 7){
-        printf ("É primo");
-    } else{
-        printf ("Não primo");
+    if (n < 2){
+        printf("Não primo");
+    }
+
+    if (((n % 2) != 0 && (n % 3) != 0 && (n % 5) != 0 && (n % 7) != 0) || n == 2 || n == 3 || n == 5 || n == 7)
+    {
+        printf("É primo");
+    }
+    else
+    {
+        printf("Não primo");
     }
     return 0;
 }
